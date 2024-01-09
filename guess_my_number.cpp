30bd46a851ebb21d55f62b3693c3df261a777e07
GiftonNdemo <giftonndemoosindi@gmail.com>
2024-01-09 15:29:03 +0300

    guess my number version 1

--- guess_my_number.cpp
+++ guess_my_number.cpp
@@ -3,6 +3,8 @@
 #include<ctime>
 using namespace std;
 int main(){
+    cout<<"Welcome to guess my number version 1!"<<endl;
+    cout<<" Here you will try to guess the secret number. Let's go!"<<endl;
     int tries=0,guess,secret_number;
     srand(static_cast<unsigned int> (time(0)));
     secret_number = rand()%100 + 1;
